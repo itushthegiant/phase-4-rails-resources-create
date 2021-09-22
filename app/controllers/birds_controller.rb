f2276a77762a2cb338358adc610c671662d4c97b
etamar <etamar.levy@gmail.com>
2021-09-22 16:15:41 -0400

    commit tree 91e1d9fb505ea9078ed257c060e02f28ee959acb

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,13 @@
 class BirdsController < ApplicationController
 
+  def create
+    # get the data from the body of the request
+    # create new bird
+    bird = Bird.create(name: params[:name], species: params[:species])
+    # send newly create Bird as a response
+    render json: bird, status: :created
+  end
+
   # GET /birds
   def index
     birds = Bird.all
